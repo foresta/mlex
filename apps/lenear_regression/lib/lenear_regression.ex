6674f648f44aa46d225db2bd108ea1e3ceb79993
kazuki morita <kz.morit@gmail.com>
2017-11-28 08:33:54 +0900

    impl lenear_regression compute cost

--- apps/lenear_regression/lib/lenear_regression.ex
+++ apps/lenear_regression/lib/lenear_regression.ex
@@ -16,6 +16,17 @@ defmodule LenearRegression do
     :world
   end
 
+  def computeCost(x, y, theta) do
+    m = length(y)
+
+    hx = Matrix.mult(x, theta)
+    hx_y = Matrix.sub(hx, y) 
+    hx_y_2 = Matrix.emult(hx_y, hx_y)
+    sum = hx_y_2
+    |> Enum.reduce(0, fn(x, acc) -> Enum.at(x,0) + acc end)
+
+    sum / (2*m)
+  end
 
 
 
--- apps/sample/lib/coursera.ex
+++ apps/sample/lib/coursera.ex
@@ -1,5 +1,36 @@
 defmodule Coursera do
 
     def ex1 do
+
+      data = DatasetLoader.load("apps/sample/dataset/coursera/ex1/ex1data1.txt")
+      
+      x = data |> Enum.map(fn x -> Enum.at(x, 0) end) |> Enum.to_list
+      y = data |> Enum.map(fn y -> Enum.at(y, 1) end) |> Enum.to_list
+
+      plotter = Explotter.new
+      Explotter.plot(plotter, x, y, :rx, [label: "data1"])
+      Explotter.show(plotter)
+
+      m = length(x)
+      
+      # バイアス項を追加
+      x = [ for times <- 1..m do 1 end | [x]]
+      x = Matrix.transpose(x)
+
+      theta = Matrix.zeros(2, 1)
+
+      iterations = 1500
+      
+      # learning rate
+      alpha = 0.01
+
+      y = Matrix.transpose([y])
+
+      cost = LenearRegression.computeCost(x, y, theta)
+      IO.puts "theta[0, 0], cost is #{cost}"
+
+      theta = [[-1], [2]]
+      cost = LenearRegression.computeCost(x,y,theta)
+      IO.puts "theta[-1,2], cost is #{cost}"
     end
 end
